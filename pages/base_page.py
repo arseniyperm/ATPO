ffba5c640079cfb78cfb3f17cc3370788628ec35
ArseniyPerm <arseniy.perm2003@gmail.com>
2025-04-21 19:46:38 +0500

    page object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from pages.locators import BasePageLocators
 
 
 class BasePage:
@@ -10,9 +13,38 @@ class BasePage:
     def open(self):
         self.browser.get(self.url)
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), \
+            "User icon is not presented," " probably unauthorised user"
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
+            return False
+        return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def goto_basket(self):
+        basket = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        basket.click()
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, [TimeoutException]).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
         return True
---
+++ test_product_page.py
@@ -0,0 +1,72 @@
+import pytest
+import time
+from pages.locators import ProductPageLocators
+from pages.locators import BasePageLocators
+from pages.product_page import ProductPage
+from pages.login_page import LoginPage
+
+
+@pytest.mark.skip
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link1 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link1)
+    page.open()
+    page.add_to_basket()
+    browser.implicitly_wait(5)
+    page.solve_quiz_and_get_code()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.goto_basket()
+    assert browser.find_element(*BasePageLocators.EMPTY_BASKET).text == "Your basket is empty. Continue shopping"
+
+
+class TestUserReg:
+    @pytest.fixture(autouse=True)
+    def test_can_reg_new_user(browser):
+        link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+        email = str(time.time()) + "@fakemail.org"
+        page = LoginPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+        page.register_new_user(email, '123455')
+        page.should_be_authorized_user()
+
+
+@pytest.mark.skip
+class TestUserAddToBasketFromProductPage:
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        assert page.is_not_element_present(*ProductPageLocators.ADDED_NTF), \
+            "Success message is presented"
+
+    def test_guest_cant_see_success_message(browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        assert not page.is_not_element_present(*ProductPageLocators.ADDED_NTF), \
+            "Success message is presented"
+
+    def test_message_disappeared_after_adding_product_to_basket(browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        assert not page.is_disappeared(*ProductPageLocators.ADDED_NTF), \
+            "Success message is disappeared"
+
+    @pytest.mark.parametrize('num', [*range(1, 7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+    def test_guest_can_add_product_to_basket(browser, num):
+        link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        browser.implicitly_wait(5)
+        page.solve_quiz_and_get_code()
